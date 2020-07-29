f0180ac8ce13ddaf564aab14846b4b325222f9ea
Ajay Prajapati <ajayprazz@gmail.com>
2020-07-29 07:37:09 +0545

    add CRUD operations

---
+++ client/src/components/streams/StreamEdit.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { connect } from "react-redux";
+import { pick } from "lodash";
+
+import { fetchStream, editStream } from "../../actions";
+
+import StreamForm from "./StreamForm";
+
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  onSubmit = formValues => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loadng...</div>;
+    }
+
+    return (
+      <div>
+        <h3>Edit a stream</h3>
+        <StreamForm
+          initialValues={pick(this.props.stream, "title", "description")}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
---
+++ client/src/reducers/streamReducer.js
@@ -0,0 +1,25 @@
+import { omit, mapKeys } from "lodash";
+import {
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  CREATE_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM,
+} from "../actions/types";
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ...mapKeys(action.payload, "id") };
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      return omit(state, action.payload);
+    default:
+      return state;
+  }
+};
